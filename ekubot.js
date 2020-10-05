3928c5a8c637637ede2db2730932576447955d71
ben-varner <72414334+ben-varner@users.noreply.github.com>
2020-10-05 15:37:18 -0500

    remove unnecessary lines and tidy up

--- ekubot.js
+++ ekubot.js
@@ -1,19 +1,15 @@
-const Discord = require('discord.js');
-const handleMessage = require('./handle/msgHandler');
-const Config = require( './config.json');
+const { token } = require( './config.json');
 
-const client = new Discord.Client();
+const client = new require('discord.js').Client();
 
-client.login(Config.token);
+client.login(token);
 
 client.on('ready', () => {
     console.log(`${client.user.tag} is online, and ready to do some work, boss!`);
 });
 
 client.on('message', (msg) => {
-    if(msg.channel.type == 'text'){
-        if(!msg.author.bot) {
-            handleMessage.handleMessage(msg, client);
-        }
+    if(msg.channel.type == 'text' && !msg.author.bot){
+        require('./handle/msgHandler').handleMessage(msg, client);
     }
 });
