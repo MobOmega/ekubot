ea39ad91c28cd6f2bca81edeae2339bbb916063c
ben-varner <72414334+ben-varner@users.noreply.github.com>
2020-10-05 15:42:17 -0500

    fix possible but and tidy up

--- handle/commands/audio playing/playAudio.js
+++ handle/commands/audio playing/playAudio.js
@@ -1,25 +1,18 @@
 const path = require('path');
 
 function play(message, audio, feedback, end, volume, notInChannel){
-    const { voice } = message.member
+    const { voice } = message.member;
 
-    if (!voice.channelID) {
-        message.channel.send(notInChannel);
-        return;
-    }
+    if (!voice.channelID) { if (notInChannel && notInChannel.trim()) { message.channel.send(notInChannel); } return; }
 
     voice.channel.join().then((connection) => {
         const dispatcher = connection.play(path.join(__dirname, audio));
         dispatcher.on('start', () => {
             dispatcher.setVolume(volume);
-            if(feedback.trim()){
-                message.channel.send(feedback);
-            }
+            if(feedback && feedback.trim()) message.channel.send(feedback);
         });
         dispatcher.on('finish', () => {
-            if(end.trim()){
-                message.channel.send(end);
-            }
+            if(end && end.trim()) message.channel.send(end);
             dispatcher.destroy();
             voice.channel.leave();
         })
